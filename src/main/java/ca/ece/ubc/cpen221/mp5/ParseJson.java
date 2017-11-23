6bf148d7f9f7da8f1afa97f1ac0756b8c0797e2c
ilikerice123 <charlesbai321@gmail.com>
2017-11-22 16:33:39 -0800

    lol

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/ParseJson.java
@@ -0,0 +1,16 @@
+package ca.ece.ubc.cpen221.mp5;
+import javax.json.*;
+
+public class ParseJson {
+
+    public ParseJson(String user, String reviews, String businesses){
+        //do something
+    }
+
+    public MP5Db<Restaurant> getRestarauntDB(){
+    }
+
+    public MP5Db<User> getUserDB(){
+        
+    }
+}
