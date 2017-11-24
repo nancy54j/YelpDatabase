daf3e44bf081af470ae295cdd6459185f2076de8
ilikerice123 <charlesbai321@gmail.com>
2017-11-24 13:42:55 -0800

    lol

--- src/main/java/ca/ece/ubc/cpen221/mp5/ParseJson.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/ParseJson.java
@@ -1,16 +1,86 @@
 package ca.ece.ubc.cpen221.mp5;
 import javax.json.*;
+import javax.json.stream.JsonParser;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ParseJson {
+    Set<Restaurant> restaurants;
+    Set<Review> reviews;
+    Set<User> users;
+    Map<String, Restaurant> restMap;
+    Map<String, User> userMap;
 
-    public ParseJson(String user, String reviews, String businesses){
-        //do something
+
+    //https://stackoverflow.com/questions/16265693/how-to-use-buffered-reader-in-java
+    public ParseJson(String users, String businesses, String reviews){
+        try {
+            //parse restaurants
+            BufferedReader restaurantReader = new BufferedReader(new FileReader(businesses));
+            String line;
+            while((line = restaurantReader.readLine()) != null){
+                JsonParser parseRestaurant = Json.createParser(new StringReader(line));
+                JsonObject restaurant = parseRestaurant.getObject();
+
+                if(restaurant.getBoolean("open")){
+                    String url = restaurant.getString("url");
+                    double longitude = restaurant.getJsonNumber("longitude").doubleValue();
+                    double latitude = restaurant.getJsonNumber("latitude").doubleValue();
+                    //TODO: OVER HERE 
+                    JsonArray a = restaurant.getJsonArray("neighborhoods")
+                }
+            }
+
+            //parse reviews
+            BufferedReader reviewReader = new BufferedReader(new FileReader(reviews));
+            while((line = reviewReader.readLine()) != null){
+                JsonParser parseReview = Json.createParser(new StringReader(line));
+                JsonObject review = parseReview.getObject();
+
+                //check if it is a date
+                if(review.getString("type").equals("review")){
+                    //convert date to unix time
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                    int unixtime = (int) (df.parse(review.getString("date")).getTime() / 1000);
+
+                    JsonObject jo = review.getJsonObject("votes");
+                    int[] upvotes = new int[] {jo.getInt("cool"), jo.getInt("useful"), jo.getInt("funny")};
+
+                    Review r = new Review(review.getJsonNumber("stars").doubleValue(),review.getString("text"),
+                            review.getString("review_id"), review.getString("user_id"),
+                            review.getString("business_id"), unixtime, upvotes);
+
+                    this.reviews.add(r);
+
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getStackTrace());
+        }
     }
 
-    public MP5Db<Restaurant> getRestarauntDB(){
+    //returns a map of unique ids to its respective restaurant
+    public Map<String, Restaurant> getRestaraunts(){
     }
 
-    public MP5Db<User> getUserDB(){
+    //returns a map of unique ids to its respective user
+    public Map<String, User> getUsers(){
         
     }
+
+    public Map<String,Set<Review>> restarauntReviews(){
+
+    }
+
+    public Map<String, Set<Review>> userReviews(){
+
+    }
+
+    public Set<Review> getReviews
 }
--- src/main/java/ca/ece/ubc/cpen221/mp5/Review.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Review.java
@@ -1,5 +1,7 @@
 package ca.ece.ubc.cpen221.mp5;
 
+import java.util.Arrays;
+
 public class Review {
     //TODO: add parameters
     public final String id;
@@ -9,16 +11,18 @@ public class Review {
     public final int date;
     public final double rating;
     private static int gen_id = 0;
+    private int[] upvotes = new int[] {0, 0, 0};
 
     //we do not need methods to return things because all of these variables are public
     //use unix encoding for date
-    public Review(double starRating, String text, String id, String user, String business, int date){
+    public Review(double starRating, String text, String id, String user, String business, int date, int[] upvotes){
         this.id = id;
         this.user = user;
         this.business = business;
         this.text = text;
         this.date = date;
         this.rating = starRating;
+        this.upvotes = Arrays.copyOf(upvotes, 3); //ensures not copy by reference and saves size
     }
 
     //create new review
@@ -28,7 +32,7 @@ public class Review {
         this.user = user_id;
         this.business = business_id;
         this.rating = starRating;
-        this.date = (int) System.currentTimeMillis() / 1000;
+        this.date = (int) (System.currentTimeMillis() / 1000);
         this.text = "";
     }
 
@@ -39,7 +43,7 @@ public class Review {
         this.user = user_id;
         this.business = business_id;
         this.rating = starRating;
-        this.date = (int) System.currentTimeMillis() / 1000;
+        this.date = (int) (System.currentTimeMillis() / 1000);
         this.text = text;
     }
 
